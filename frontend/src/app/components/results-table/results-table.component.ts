cb4d7e2f2166eab6aace57ac7910b3d1d4d03673
HuntTheShunt11 <m.prouheze@laposte.net>
2026-02-07 09:54:17 +0100

    Add missing translations

--- frontend/src/app/components/results-table/results-table.component.ts
+++ frontend/src/app/components/results-table/results-table.component.ts
@@ -1,13 +1,15 @@
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 import { Incident } from '../../api.service';
 import {DatePipe} from '@angular/common';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-results-table',
   templateUrl: './results-table.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
-    DatePipe
+    DatePipe,
+    TranslatePipe
   ],
   styleUrls: ['./results-table.component.css']
 })
